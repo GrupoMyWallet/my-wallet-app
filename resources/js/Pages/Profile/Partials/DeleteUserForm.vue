a212d633586426ca2d1b9f73194b18669fc65f3e
Eduardo Henrique <dudugama03@gmail.com>
2025-06-11 10:50:00 -0300

    Mudando texto em ingles das pages de profile e retirando funcionalidades desnecessárias

--- resources/js/Pages/Profile/Partials/DeleteUserForm.vue
+++ resources/js/Pages/Profile/Partials/DeleteUserForm.vue
@@ -40,21 +40,21 @@ const closeModal = () => {
 <template>
     <ActionSection>
         <template #title>
-            Delete Account
+           Excluir Conta
         </template>
 
         <template #description>
-            Permanently delete your account.
+            Excluir sua conta permanentemente.
         </template>
 
         <template #content>
             <div class="max-w-xl text-sm text-gray-600">
-                Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.
+                Depois que sua conta for excluída, todos os dados também serão permanentemente excluídos.
             </div>
 
             <div class="mt-5">
                 <DangerButton @click="confirmUserDeletion">
-                    Delete Account
+                    Excluir Conta
                 </DangerButton>
             </div>
 
@@ -65,7 +65,7 @@ const closeModal = () => {
                 </template>
 
                 <template #content>
-                    Are you sure you want to delete your account? Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.
+                    Tem certeza que quer excluir sua conta? Depois que sua conta for excluída, todos os dados também serão permanentemente excluídos. Coloque sua senha para confirmar a exxclusão de sua conta.
 
                     <div class="mt-4">
                         <TextInput
@@ -84,7 +84,7 @@ const closeModal = () => {
 
                 <template #footer>
                     <SecondaryButton @click="closeModal">
-                        Cancel
+                        Cancelar
                     </SecondaryButton>
 
                     <DangerButton
@@ -93,7 +93,7 @@ const closeModal = () => {
                         :disabled="form.processing"
                         @click="deleteUser"
                     >
-                        Delete Account
+                        Excluir Conta
                     </DangerButton>
                 </template>
             </DialogModal>
