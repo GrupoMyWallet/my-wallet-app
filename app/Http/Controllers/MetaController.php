093c3e092db1b3ae1b9e091febe2e4f90604528e
Eduardo Henrique <dudugama03@gmail.com>
2025-06-11 20:59:20 -0300

    Adicionando metodos de update e delete no controller e no repository, colocando app debug=true no env.exemple e adicnando ignores no .gitignore

--- app/Http/Controllers/MetaController.php
+++ app/Http/Controllers/MetaController.php
@@ -5,11 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Meta;
+use App\Repositories\MetaRepository;
 use Inertia\Inertia;
 use Carbon\Carbon;
 
 class MetaController extends Controller
-{
+{   
+    protected $metaRepository;
+
+    public function __construct(metaRepository $metaRepository)
+    {
+        $this->metaRepository = $metaRepository;
+    }
+
     public function index(Request $request)
     {   
         $metas = Meta::all();
@@ -40,6 +48,48 @@ class MetaController extends Controller
         $meta = Meta::create($validated);
 
         return redirect()->route('metas.index')->with('success', 'Meta cadastrada com sucesso!');
+    }
+
+    public function update(Request $request, $id)
+    {   
+        try {
+            $validated = $request->validate([
+                'titulo'             => 'required|string|max:255',
+                'descricao'          => 'required|string',
+                'valor_a_alcancar'   => 'required|numeric|min:0',
+                'valor_atual'        => 'nullable|numeric|min:0',
+                'data_final'         => 'nullable|date_format:d/m/Y',
+                'status'             => 'required|in:pendente,andamento,completa,cancelada',
+            ]);
+
+            $this->metaRepository->update($id, $validated);
+
+            return redirect()->route('metas.index')->with('success', 'Meta atualizada com sucesso!');
+        } catch (\Illuminate\Validation\ValidationException $e) {
+
+            return back()->withErrors($e->errors())->withInput();
+        } catch (\Exception $e) {
+            
+            return redirect()->route('metas.index')->with('error', 'Não foi possível atualizar a meta. Tente novamente mais tarde.');
+        }
+        
+    
+        
+    }
+
+    public function destroy($id)
+    {
+        try {
+        
+            $this->metaRepository->delete($id);
+    
+            return redirect()->route('metas.index')->with('success', 'Meta excluída com sucesso!');
+        } catch (\Exception $e) {
+            // Você pode logar o erro se quiser
+            // \Log::error("Erro ao excluir categoria: " . $e->getMessage());
+    
+            return redirect()->route('metas.index')->with('error', 'Não foi possível excluir a Meta. Tente novamente mais tarde.');
+        }
     }
 
 }
---
+++ app/Repositories/MetaRepository.php
@@ -0,0 +1,49 @@
+<?php 
+
+namespace App\Repositories;
+
+use App\Models\Meta;
+
+class MetaRepository
+{   
+    protected $model;
+
+    public function __construct(Meta $meta)
+    {
+        $this->model = $meta;
+    }
+    
+    public function getCategoriaDoUsuario($userId)
+    {
+        return $this->model::where(function ($query) use ($userId) {
+            $query->where('user_id', $userId)
+            ->orWhereNull('user_id');      
+        })->get();
+    }
+
+
+    public function findOrFail($id)
+    {
+        return $this->model::findOrFail($id);
+    }
+
+    public function update(int $id, array $data)
+    {
+        $categoria = $this->model->findOrFail($id);
+        $categoria->update($data);
+        return $categoria;
+    }
+
+    public function delete(int $id)
+    {
+        return $this->model->findOrFail($id)->delete();
+    }
+
+    public function getCategoriaDespesas()
+    {
+        return $this->model
+            ->where('tipo', 'despesa')
+            ->orderBy('nome')
+            ->get();
+    }
+}
