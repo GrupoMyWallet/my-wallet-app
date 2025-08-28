df7df8e729026a79d3c82053eb4289ebb72519c7
jucorreab <juliacorrea11@gmail.com>
2025-08-28 17:06:00 -0300

    feat: ajustes de senha e de reset de senha

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -29,7 +29,8 @@ const submit = () => {
         </template>
 
         <div class="mb-4 text-sm text-gray-600">
-            Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.
+            Esqueceu sua senha? Sem problemas. 
+            <br>Informe seu e-mail e enviaremos um link para redefinição da senha.
         </div>
 
         <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
@@ -53,7 +54,7 @@ const submit = () => {
 
             <div class="flex items-center justify-end mt-4">
                 <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Email Password Reset Link
+                    Continuar
                 </PrimaryButton>
             </div>
         </form>
