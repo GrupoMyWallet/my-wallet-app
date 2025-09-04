2ccd9fd86779b7d7ecae55d8f8017996e2ee9112
jucorreab <juliacorrea11@gmail.com>
2025-09-04 18:12:06 -0300

    Adiciona link para registro na tela de login e ajustes visuais

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -77,15 +77,25 @@ const submit = () => {
                 </label>
             </div>
 
+            
             <div class="flex items-center justify-end mt-4">
                 <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     Esqueceu a senha?
                 </Link>
 
+            <div class="flex items-center">
+                <Link :href="route('register')" 
+                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ms-4">
+                    Não tem conta? Crie uma
+                </Link>
+
+            </div>
+            
                 <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                     Log in
                 </PrimaryButton>
             </div>
+
         </form>
     </AuthenticationCard>
 </template>
